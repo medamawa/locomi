8b1f72d1016c86a8f259e86a5b650642b322cf0e
SamidareGeorge <sogo116@icloud.com>
2020-04-27 11:13:50 +0900

    Initial Commit

---
+++ locomi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  locomi
+//
+//  Created by sogo on 2020/04/27.
+//  Copyright © 2020 Sogo Nishihara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
